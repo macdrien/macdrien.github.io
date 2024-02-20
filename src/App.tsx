2405906252547cbb5882da68c71e7fff5096233c
macdrien <adrien.bouyssou@gmail.com>
2024-02-20 08:29:22 +0100

    FIX Some errors and formatting

--- src/App.tsx
+++ src/App.tsx
@@ -39,10 +39,10 @@ function App() {
             <h1>Salut,<span>je suis <span className="highlight">Adrien BOUYSSOU</span></span></h1>
             <p className="welcomeSubtitle">Full-stack web developpeur chez Viveris Technologies</p>
             <p className="welcomeDescription">Ingénieur logiciel depuis 3 ans, je travaille principalement avec Java et
-              ReactJS. J’ai rejoins Viveris
-              Technologies (Toulouse, France) après mon diplôme en 2021. Je travaille là-bas en tant qu’ingénieur
-              full-stack. Je travaille également toujours sur des side-projects sur mon temps libre.</p>
-            <button className="contactMe" onClick={goToWork}>Contact me</button>
+              ReactJS. J’ai rejoint Viveris Technologies (Toulouse, France) après mon diplôme en 2021.
+              J'y travaille comme ingénieur full-stack.
+              Je travaille également sur des side-projects sur mon temps libre.</p>
+            <button className="contactMe" onClick={goToWork}>Contactez-moi</button>
           </div>
           <img src={landingLogo} alt="alt message" className="landingLogo"/>
           <div className="checkout">
@@ -69,24 +69,20 @@ function App() {
               <div className="description">
                 <p>Depuis 2021, je travaille chez <span className="highlight">Viveris Technologies</span>. Basé à
                   Toulouse, j'occupe le poste de développeur <span className="highlight">full-stack</span> au sein d'une
-                  équipe agile. Mon travail change au fil des jours, des semaines et des
-                  sprints.</p>
+                  équipe agile. Mon travail change au fil des jours, des semaines et des sprints.</p>
                 <p>Mes compétences principales restent le développement web, en front-end avec <span
                   className='highlight'>VueJS</span> que nous introduisons
-                  dans le projet, et en back-end avec une API en <span className="highlight">Spring</span>. Mais, il
-                  m'arrive également de travailler sur les
-                  bases de données, faire du réseau ou encore de l'administration système et travailler sur la CI du
-                  projet.</p>
+                  dans le projet, et en back-end avec une API principale en <span className="highlight">Spring</span>.
+                  Mais, il m'arrive également de travailler sur les bases de données,
+                  faire du réseau ou encore de l'administration système et travailler sur la CI du projet.</p>
               </div>
             </div>
             <div className="experience">
               <div className="illustration"></div>
               <div className="description">
-                <p>De 2018 à 2021, j'ai fait une école d'ingénieur en alternance. Je travaillais pour <span
+                <p>De 2018 à 2021, j'ai fait une école d'ingénieur en alternance. Je travaillais chez <span
                   className="highlight">Eiffage-Energie-Systèmes</span> et j'étudiais au sein de l'<span
-                  className="highlight">UTBM</span> (Université
-                  de Technologie de
-                  Belfort-Montbéliard).<br/>
+                  className="highlight">UTBM</span> (Université de Technologie de Belfort-Montbéliard).<br/>
                   Je travaillais à Mulhouse, en Alsace, en tant que développeur web avec du <span
                     className="highlight">ReactJS</span> en framework front-end
                   et en <span className="highlight">Spring Boot</span> pour le back-end.<br/>
@@ -107,15 +103,11 @@ function App() {
                 Pour un étudiant en cursus initial, cette expérience se concrétise souvent par un semestre d'étude à
                 l'étranger.
                 Mais, étant alternant, j'ai eu accès à un autre type de parcours international: douze semaines de
-                travail
-                à l'étranger.<br/>
+                travail à l'étranger.<br/>
                 Le premier mois pris la forme d'un stage linguistique comprenant une semaine de cours en Angleterre,
-                dans
-                le Devon plus précisément.
-                S'en suivi trois semaines de travail dans une société locale.<br/>
+                dans le Devon plus précisément. S'en suivi trois semaines de travail dans une société locale.<br/>
                 A la fin de ce premier mois, j'ai eu l'opportunité de retourner dans cette même entreprise pour faire
-                les
-                deux mois de travail qu'il me restait à faire.
+                les deux mois de travail qu'il me restait à faire.
               </p>
             </div>
           </div>
